3e716fe74bcfcc7e5458e291fce353eef4ef2e95
Edmund White <Edmund.White@hotmail.com>
2013-02-13 01:12:04 +0000

    Added ContactTest.java
    Class testing the implementation of the Contact class.

---
+++ ContactTest.java
@@ -0,0 +1,44 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class ContactTest{
+	
+	@Test
+	public void TestsGetId(){
+		int testSize = 100;
+		Contact[] testContact = new ContactImpl[testSize];
+		for(int c = 0;c<testSize;c++){
+			testContact[c] = new ContactImpl(c,"" + c);
+		}
+		for(int c = 0;c<testSize;c++){
+			assertEquals(c,testContact[c].getId());
+		}
+	}
+	
+	@Test
+	public void TestsGetName(){
+		int testSize = 100;
+		Contact[] testContact = new ContactImpl[testSize];
+		for(int c = 0;c<testSize;c++){
+			testContact[c] = new ContactImpl(c,"" + c);
+		}
+		for(int c = 0;c<testSize;c++){
+			assertTrue(("" + c).equals(testContact[c].getName()));
+		}
+	}
+	
+	@Test
+	public void TestsAddGetNotes(){
+		int testSize = 100;
+		Contact[] testContact = new ContactImpl[testSize];
+		String[] randomList = new String[100];
+		for(int c = 0;c<testSize;c++){
+			testContact[c] = new ContactImpl(c,"" + c);
+			randomList[c] = "" + Math.random();
+			testContact[c].addNotes("" + randomList[c]);
+		}
+		for(int c = 0;c<testSize;c++){
+			assertEquals(randomList[c],testContact[c].getNotes());
+		}
+	}
+}
