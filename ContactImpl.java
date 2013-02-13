5bcc62d38131588067d2ff9f7c9c5550463a1ec2
Edmund White <Edmund.White@hotmail.com>
2013-02-13 01:16:17 +0000

    Added ContactImpl
    Implementation (initial) of the Contact interface.

---
+++ ContactImpl.java
@@ -0,0 +1,37 @@
+public Class ContactImpl implements Contact {
+	/**
+	* Returns the ID of the contact.
+	*
+	* @return the ID of the contact.
+	*/
+	int getId(){
+		return 0;
+	}
+	/**
+	* Returns the name of the contact.
+	*
+	* @return the name of the contact.
+	*/
+	String getName(){
+		return "";
+	}
+	/**
+	* Returns our notes about the contact, if any.
+	*
+	* If we have not written anything about the contact, the empty
+	* string is returned.
+	*
+	* @return a string with notes about the contact, maybe empty.
+	*/
+	String getNotes(){
+		return "";
+	}
+	/**
+	* Add notes about the contact.
+	*
+	* @param note the notes to be added
+	*/
+	void addNotes(String note){
+		;
+	}
+}
