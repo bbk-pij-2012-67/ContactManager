6c316266002d792b8eb1a1b74c5f6cbc2fb87265
Edmund White <Edmund.White@hotmail.com>
2013-02-16 15:30:18 +0000

    Refactoring and minor formatting
    Refactored ContactTest.java to initialise objects. Minor formatting changes in Meeting.java.

--- ContactTest.java
+++ ContactTest.java
@@ -3,37 +3,49 @@ import static org.junit.Assert.*;
 
 public class ContactTest{
 	
-	@Test
-	public void TestsGetId(){
-		int testSize = 100;
-		Contact[] testContact = new ContactImpl[testSize];
+	private int testSize;
+	private Contact[] testContact;
+	private String[] randomList;
+	
+	@Before
+	public void initialise(){
+		testSize = 100;
+		testContact = new ContactImpl[testSize];
+		randomList = new String[100];
 		for(int c = 0;c<testSize;c++){
 			testContact[c] = new ContactImpl(c,"" + c);
 		}
+	}
+	
+	@Test
+	public void testsGetId(){
+		//int testSize = 100;
+		//Contact[] testContact = new ContactImpl[testSize];
+		//for(int c = 0;c<testSize;c++){
+		//	testContact[c] = new ContactImpl(c,"" + c);
+		//}
 		for(int c = 0;c<testSize;c++){
 			assertEquals(c,testContact[c].getId());
 		}
 	}
 	
 	@Test
-	public void TestsGetName(){
-		int testSize = 100;
-		Contact[] testContact = new ContactImpl[testSize];
-		for(int c = 0;c<testSize;c++){
-			testContact[c] = new ContactImpl(c,"" + c);
-		}
+	public void testsGetName(){
+		//int testSize = 100;
+		//Contact[] testContact = new ContactImpl[testSize];
+		//for(int c = 0;c<testSize;c++){
+		//	testContact[c] = new ContactImpl(c,"" + c);
+		//}
 		for(int c = 0;c<testSize;c++){
 			assertTrue(("" + c).equals(testContact[c].getName()));
 		}
 	}
 	
 	@Test
-	public void TestsAddGetNotes(){
-		int testSize = 100;
-		Contact[] testContact = new ContactImpl[testSize];
-		String[] randomList = new String[100];
+	public void testsAddGetNotes(){
+		
 		for(int c = 0;c<testSize;c++){
-			testContact[c] = new ContactImpl(c,"" + c);
+			//testContact[c] = new ContactImpl(c,"" + c);
 			randomList[c] = "" + Math.random();
 			testContact[c].addNotes("" + randomList[c]);
 		}
@@ -45,5 +57,12 @@ public class ContactTest{
 		// test for no notes saved.
 		Contact testContact2 = new ContactImpl(99999,"a");
 		assertEquals("",testContact2.getNotes());
+	}
+	
+	@After
+	public void cleanUp(){
+		testSize = 0;
+		testContact = null;
+		randomList = null;
 	}
 }
--- Meeting.java
+++ Meeting.java
@@ -13,18 +13,20 @@ public interface Meeting {
 	* @return the id of the meeting.
 	*/
 	int getId();
+	
 	/**
 	* Return the date of the meeting.
 	*
 	* @return the date of the meeting.
 	*/
 	Calendar getDate();
+	
 	/**
 	* Return the details of people that attended the meeting.
 	*
 	* The list contains a minimum of one contact (if there were
 	* just two people: the user and the contact) and may contain an
-	* arbitraty number of them.
+	* arbitrary number of them.
 	*
 	* @return the details of people that attended the meeting.
 	*/
