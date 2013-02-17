5be405890a58074a197de665243feb7bb9446e4e
Edmund White <Edmund.White@hotmail.com>
2013-02-17 22:06:43 +0000

    Added MeetingImpl.java

---
+++ MeetingImpl.java
@@ -0,0 +1,59 @@
+import java.util.Calendar;
+import java.util.Set;
+
+/**
+* A class to represent meetings
+*
+* Meetings have unique IDs, scheduled date and a list of participating contacts
+*/
+public class MeetingImpl implements Meeting{
+	
+	private int id;
+	private Calendar date;
+	private Set<Contact> contacts;
+	
+	/**
+	* Constructor creates a new MeetingImpl object.
+	*
+	* @param id int value of the meeting ID.
+	* @param date Calendar object storing the date of the meeting.
+	* @param contacts Set<Contact> object storing a list of contacts for the meeting.
+	*/
+	public MeetingImpl(int id, Calendar date, Set<Contact> contacts){
+		this.id = id;
+		this.date = date;
+		this.contacts = contacts;
+	}
+	
+	/**
+	* Returns the id of the meeting.
+	*
+	* @return the id of the meeting.
+	*/
+	public int getId(){
+		return id;
+	}
+		
+	
+	/**
+	* Return the date of the meeting.
+	*
+	* @return the date of the meeting.
+	*/
+	public Calendar getDate(){
+		return date;
+	}
+	
+	/**
+	* Return the details of people that attended the meeting.
+	*
+	* The list contains a minimum of one contact (if there were
+	* just two people: the user and the contact) and may contain an
+	* arbitrary number of them.
+	*
+	* @return the details of people that attended the meeting.
+	*/
+	public Set<Contact> getContacts(){
+		return contacts;
+	}
+}
