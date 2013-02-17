94e3bf905d2bb6bacf85f9eb797029d3408a7514
Edmund White <Edmund.White@hotmail.com>
2013-02-17 18:50:16 +0000

    Modified testsGetContacts
    Changed test to check that the contacts set object added to each meeting is the same as what is returned from getContacts()

--- MeetingTest.java
+++ MeetingTest.java
@@ -1,34 +1,38 @@
 import org.junit.*;
 import static org.junit.Assert.*;
 import java.util.Calendar;
+import java.util.Set;
+import java.util.TreeSet;
 
 public class MeetingTest{
 	
 	private int testSize;
 	private Meeting[] testMeeting;
-	private Set<Contact> testContact;
-	private CalendarData calData;
+	private Set<Contact> testContacts;
+	private Calendar[] calArray;
 	
 	@Before
 	public void initialise(){
 		testSize = 100;
 		testMeeting = new MeetingImpl[testSize];
-		testContact = new ContactImpl[10];
+		//Create a set of contacts
+		testContacts = new TreeSet<Contact>;
 		for(int c = 0;c < 10;c++){
-			testContact.add(new ContactImpl(c,"" + c));
+			testContacts.add(new ContactImpl(c,"" + c));
 		}
 		//randomList = new String[100];
 		//Add test dates to the meetings. Store the dates in an
 		//array.
 		for(int c = 0;c<testSize;c++){
 			Calendar cal = Calendar.getInstance();
-			calData = new CalendarData[testSize];
+			calArray = new Calendar[testSize];
 			int year = (int)(Math.random * 5)+2013;
 			int month = (int)(Math.random * 12)+ 1;
 			int day = (int)(Math.random * 28)+ 1;
-			calData[c] = new CalendarData(year,month,day);
+			calArray[c] = Calendar.getInstance();
+			calArray[c].set(year,month,day);
 			cal.set(year,month,day)
-			testMeeting[c] = new MeetingImpl(c,cal,testContact);
+			testMeeting[c] = new MeetingImpl(c,cal,testContacts);
 		}
 	}
 	
@@ -41,17 +45,18 @@ public class MeetingTest{
 	
 	@Test
 	public void testsGetDate(){
-	}
-	
-	private class CalendarData{
-		public int _year;
-		public int _month;
-		public int _day;
-	
-		public CalendarDate(int year, int month, int, day){
-			this._year=year;
-			this._month=month;
-			this._day=day;
+		for(int c = 0;c<testSize;c++){
+			assertEquals(calArray[c].equals(testMeeting[c].getDate()));
 		}
 	}
+	
+	@Test
+	public void testsGetContacts(){
+		for(int c = 0;c<testSize;c++){
+			assertTrue(testMeeting[c].getContacts()==testContacts);
+		}
+	}
+}
+	
+	
 	
