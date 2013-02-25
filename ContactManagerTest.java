3fb2eeb91064c722e51a50fd95d60324c162afd5
Edmund White <Edmund.White@hotmail.com>
2013-02-25 13:03:33 +0000

    Added a cleanup method on ContactManagerTest.

--- ContactManagerTest.java
+++ ContactManagerTest.java
@@ -2,13 +2,13 @@ import org.junit.*;
 import static org.junit.Assert*;
 
 public class ContactManagerTest{
-	
+
 	private Set<Contact> testContacts;
 	private Calendar[] calArray;
 	private int testSize;
 	ContactManager cm
 	private int[] futureMeetingIDs;
-	
+
 	@Before
 	public void initialize(){
 		testSize = 100;
@@ -19,11 +19,11 @@ public class ContactManagerTest{
 		for(int c = 0;c < 10;c++){
 			testContacts.add(new ContactImpl(c,"" + c));
 		}
-		
+
 		//Store some dates in an array.
 		calArray = new Calendar[testSize];
 		for(int c = 0;c<testSize;c++){
-			
+
 			//Create a random date
 			int year = (int)(Math.random() * 5)+2013;
 			int month = (int)(Math.random() * 12)+ 1;
@@ -31,24 +31,33 @@ public class ContactManagerTest{
 			calArray[c] = Calendar.getInstance();
 			calArray[c].set(year,month,day);
 		}
-		
-		
+
+
 		//Create FutureMeeting objects
 		for(int c = 0; c < testSize; c++){
-			futureMeetingIDs[c] = cm.addFutureMeeting(testContacts,calArray[c])
+			futureMeetingIDs[c] = cm.addFutureMeeting(testContacts,calArray[c]);
 		}
 	}
-	
+
 	@Test
 	public void TestsGetFutureMeeting(){
 		FutureMeeting fm;
 		for(int c = 0; c < testSize; c++){
 			fm = cm.getFutureMeeting(futureMeetingIDs[c]);
-			
+
 			assertEquals(fm.getContacts(),testContacts);
 		}
 		//test for non-existent id
 		fm = cm.getFutureMeeting(-1);
 		assertEquals(fm,null);
+	}
+
+
+	@After
+	public void CleanUp(){
+		testContacts = null;
+		calArray= null;
+		cm = null;
+		futureMeetingIDs = null;
 	}
 }
